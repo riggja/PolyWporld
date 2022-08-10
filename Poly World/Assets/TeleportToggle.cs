06899a33313bd6dff2c3afdc90a0de449d340726
riggja <riggja98@gmail.com>
2022-08-09 20:07:47 -0500

    saving

---
+++ Poly World/Assets/TeleportToggle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+using System;
+
+public class TeleportToggle : MonoBehaviour
+{
+    [SerializeField] private InputActionReference teleportToggleBtn;
+    
+   public UnityEvent OnTeleportActivate;
+   public UnityEvent OnTeleportCancel;
+
+   private void onEnable()
+   {
+        teleportToggleBtn.action.performed += ActivateTeleport;
+        teleportToggleBtn.action.canceled += DeactivateTeleport;
+
+   }
+
+   private void onDisable()
+   {
+        teleportToggleBtn.action.performed -= ActivateTeleport;
+        teleportToggleBtn.action.canceled -= DeactivateTeleport;
+
+   }
+
+    private void ActivateTeleport(InputAction.CallbackContext obj) => OnTeleportActivate.Invoke();
+    private void DeactivateTeleport(InputAction.CallbackContext obj) => Invoke("TurnOffTeleport", .1f);
+    private void TurnOffTeleport() => OnTeleportCancel.Invoke();
+
+
+}
